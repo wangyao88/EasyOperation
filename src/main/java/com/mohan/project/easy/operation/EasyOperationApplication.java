0879dbe47a82ce89cea4842c211212b51acb7251
wangyao <wangyao@cis.com.cn>
2019-11-05 08:50:18 +0800

    项目初始化

---
+++ src/main/java/com/mohan/project/easy/operation/EasyOperationApplication.java
@@ -0,0 +1,13 @@
+package com.mohan.project.easy.operation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasyOperationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasyOperationApplication.class, args);
+	}
+
+}
